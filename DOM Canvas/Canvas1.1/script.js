06f7b3c9280281395d0e5d9cc57f41a3c6f27d56
AmtoJohn <amto.john@iisgalvanimi.edu.it>
2021-11-11 10:59:07 +0000

    Esercitazione-gitpod4.0

---
+++ DOM Canvas/Canvas1.1/script.js
@@ -0,0 +1,8 @@
+function button1Function() {
+    document.getElementById("mytext").innerHTML = "Hello World";
+    document.getElementById("mytext2").innerHTML = "Sono il secondo testo";
+  }
+function button2Function() {
+    document.getElementById("mytext").innerHTML = "Mi piace javascript";
+    document.getElementById("mytext2").innerHTML = "A me non piace javascript";
+}
